9116192dd8465a7a2b154692720c0e3d86e5c4e0
jinxiaomei <18253591318@163.com>
2016-04-12 11:10:53 +0800

    Initial Commit

---
+++ 04-12swift控件使用/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  04-12swift控件使用
+//
+//  Created by pengli on 16/4/12.
+//  Copyright © 2016年 jinxiaomei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
